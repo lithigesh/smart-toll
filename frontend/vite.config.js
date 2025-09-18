f18ef2629111f56822e54b49dbd9d0dc375d7702
lithigesh <lithigesh@gmail.com>
2025-09-18 23:35:28 +0530

    Shadcn UI config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@/components": fileURLToPath(new URL("./src/components", import.meta.url)),
+      "@/lib": fileURLToPath(new URL("./src/lib", import.meta.url)),
+    },
+  },
 })
